6464aa52178336ac12049d73770ce0a7e5e4ab99
mdwikuntobayu <mdwikuntobayu@gmail.com>
2012-04-16 00:55:42 +0700

    start project

---
+++ prjkunto/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prjkunto::Application.config.secret_token = '90d55d3f60644fd30bd39f583300314ca21a6ccc7a0d857ee2c86b260a971777c87f83abbe46510de4bdcfe3a914b6d28ac78c2aa6c50497ab9f4934c64e0e22'
